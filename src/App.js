e903e5b3ca8a052e5069d300d81b7ea8151bde56
kadri Awatef <awatefkadri@yahoo.com>
2021-07-19 11:31:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import myWonderfulImage  from "./myimage.jpg" ;
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+  <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+    <h1 className="title red"> My Name Is Awatef kadri </h1>
+    <img src="/myImage.jpg" alt="myimage"width={300} height={250} />  
+    <img className="myimage"    src={myWonderfulImage } alt ='myImage'width={300} height={250}  />
+    
+  </div>
+  <video width={320} height={240} controls>
+    <source src="myVideo.mp4" type="video/mp4" />
+  </video>
+</>
     </div>
   );
 }
